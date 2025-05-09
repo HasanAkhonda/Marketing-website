c0d09fb00565d92dfa98eb6700294acc24f03f76
hasanakhonda <mdmahmudhasanakhonda@gmail.com>
2025-05-09 17:26:26 +0600

    all 6 section covered

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
+import BoostYourOnlinePresence from "@/components/BoostYourOnlinePresence/BoostYourOnlinePresence";
 import ContactUs from "@/components/ContactUsSection/ContactUs";
 import HeroSection from "@/components/heroSection/HeroSection";
-import NavigationBar from "@/components/navigation/NavigationBar";
-import Portfolio from "@/components/PortfolioSection/Portfolio";
+import DifferentSection from "@/components/howAreWeDifferent/DifferentSection";
+import IncreaseROIToday from "@/components/IncreaseROItoday/IncreaseROIToday";
+import Portfolio from "@/components/portfolioSection/Portfolio";
 
 
 export default function Home() {
   return (
-    <div className="flex flex-col max-w-[1920px]  mx-auto">
+    <div className="flex flex-col mx-auto">
       <HeroSection/>
       <ContactUs/>
       <Portfolio/>
+      <DifferentSection/>
+      <BoostYourOnlinePresence/>
+      <IncreaseROIToday/>
     </div>
   );
 }
--- components/PortfolioSection/Portfolio.tsx
+++ components/PortfolioSection/Portfolio.tsx
@@ -5,14 +5,14 @@ const Portfolio = () => {
     return (
         <section id='portfolio'
             className="w-full bg-[#21184a]"
-
+ 
         >
-            <div className=" text-white rounded shadow-md max-w-5xl mx-5 md:mx-auto  md:my-24 ">
-                <h2 className="font-bold md:text-center  text-[32px] sm:text-[40px] my-8 md:text-[48px] text-start">
+            <div className=" text-gray-50 rounded shadow-md max-w-5xl mx-5 md:mx-auto  md:my-24 ">
+                <h2 className="font-bold text-white md:text-center  text-[32px] sm:text-[40px] my-6 md:text-5xl text-start">
                     Our Portfolio
                 </h2>
                 
-                <section className='flex flex-col md:flex-row m-auto gap-20'>
+                <section className='flex flex-col md:flex-row md: justify-center gap-16'>
                     <div className="flex m-auto w-full md:w-1/3 border-8 border-white/40 rounded-3xl">
                         <Image
                             className="aspect-square  w-full h-full object-cover rounded-2xl"
@@ -23,26 +23,26 @@ const Portfolio = () => {
                         />
                     </div>
 
-                    <div className='md:w-2/3 w-full'>
-                        <h2 className="flex flex-start font-bold text-center  text-[32px] sm:text-[40px] md:text-[48px] mb-12">
+                    <div className='md:w-2/3 w-full h-fit my-auto'>
+                        <h2 className="flex flex-start font-semibold text-center my-6 text-4xl sm:text-[40px] md:text-5xl">
                             Portfolio
                         </h2>
-                        <p > Ergonomics Advance specializes in providing ergonomic assessments and evaluations to enhance workplace comfort and reduce injuries.  Their certified specialists analyze employees' behaviors, office spaces, and equipment to offer tailored recommendations aimed at improving productivity and minimizing discomfort.  Services include office ergonomic assessments, industrial ergonomics, dental ergonomics, and ergonomics training.  They serve various locations across California, including Beverly Hills, El Segundo, Hollywood, and Studio City.</p>
+                        <p className='font-light '> Ergonomics Advance specializes in providing ergonomic assessments and evaluations to enhance workplace comfort and reduce injuries.  Their certified specialists analyze employees' behaviors, office spaces, and equipment to offer tailored recommendations aimed at improving productivity and minimizing discomfort.  Services include office ergonomic assessments, industrial ergonomics, dental ergonomics, and ergonomics training.  They serve various locations across California, including Beverly Hills, El Segundo, Hollywood, and Studio City.</p>
                         <a
                             href="#sass"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="inline-block mt-8 bg-white text-[#1A0542] px-5 py-2 rounded-md font-semibold hover:bg-gray-100 transition mb-6"
+                            className="inline-block w-fit bg-white my-6 text-[#1A0542] px-5 py-3 rounded-sm font-semibold hover:bg-gray-100 transition"
                         >
                             View Website
                         </a>
                     </div>
                 </section>
-                <span className="flex items-center">
+                <span className="flex items-center my-6">
                     <span className="h-px flex-1 bg-white/30"></span>
                 </span>
-                <section className='flex flex-col md:flex-row m-auto gap-20'>
-                    <div className="flex m-auto w-full md:w-1/3 border-8 border-white/40 mt-6 rounded-3xl">
+                <section className='flex flex-col md:flex-row md: justify-center gap-16'>
+                    <div className="flex m-auto w-full md:w-1/3 border-8 border-white/40 rounded-3xl">
                         <Image
                             className="aspect-square  w-full h-full object-cover rounded-2xl"
                             src="/Portfolio1.jpg"
@@ -52,26 +52,23 @@ const Portfolio = () => {
                         />
                     </div>
 
-                    <div className='md:w-2/3 w-full'>
-                        <h2 className="flex flex-start font-bold text-center  text-[32px] sm:text-[40px] md:text-[48px] mb-12">
-                            Portfolio
-                        </h2>
-                        <p > Ergonomics Advance specializes in providing ergonomic assessments and evaluations to enhance workplace comfort and reduce injuries.  Their certified specialists analyze employees' behaviors, office spaces, and equipment to offer tailored recommendations aimed at improving productivity and minimizing discomfort.  Services include office ergonomic assessments, industrial ergonomics, dental ergonomics, and ergonomics training.  They serve various locations across California, including Beverly Hills, El Segundo, Hollywood, and Studio City.</p>
+                    <div className='md:w-2/3 w-full h-fit my-auto'>
+                        <p className='font-light '> Ergonomics Advance specializes in providing ergonomic assessments and evaluations to enhance workplace comfort and reduce injuries.  Their certified specialists analyze employees' behaviors, office spaces, and equipment to offer tailored recommendations aimed at improving productivity and minimizing discomfort.  Services include office ergonomic assessments, industrial ergonomics, dental ergonomics, and ergonomics training.  They serve various locations across California, including Beverly Hills, El Segundo, Hollywood, and Studio City.</p>
                         <a
                             href="#sass"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="inline-block mt-8 bg-white text-[#1A0542] px-5 py-2 rounded-md font-semibold hover:bg-gray-100 transition mb-6"
+                            className="inline-block w-fit bg-white my-6 text-[#1A0542] px-5 py-3 rounded-sm font-semibold hover:bg-gray-100 transition"
                         >
                             View Website
                         </a>
                     </div>
                 </section>
-                <span className="flex items-center">
+                <span className="flex items-center my-6">
                     <span className="h-px flex-1 bg-white/30"></span>
                 </span>
-                <section className='flex flex-col md:flex-row m-auto gap-20'>
-                    <div className="flex m-auto w-full md:w-1/3 border-8 border-white/40 rounded-3xl mt-6">
+                <section className='flex flex-col md:flex-row md: justify-center gap-16'>
+                    <div className="flex m-auto w-full md:w-1/3 border-8 border-white/40 rounded-3xl">
                         <Image
                             className="aspect-square  w-full h-full object-cover rounded-2xl"
                             src="/Portfolio1.jpg"
@@ -81,21 +78,18 @@ const Portfolio = () => {
                         />
                     </div>
 
-                    <div className='md:w-2/3 w-full'>
-                        <h2 className="flex flex-start font-bold text-center  text-[32px] sm:text-[40px] md:text-[48px] mb-12">
-                            Portfolio
-                        </h2>
-                        <p > Ergonomics Advance specializes in providing ergonomic assessments and evaluations to enhance workplace comfort and reduce injuries.  Their certified specialists analyze employees' behaviors, office spaces, and equipment to offer tailored recommendations aimed at improving productivity and minimizing discomfort.  Services include office ergonomic assessments, industrial ergonomics, dental ergonomics, and ergonomics training.  They serve various locations across California, including Beverly Hills, El Segundo, Hollywood, and Studio City.</p>
+                    <div className='md:w-2/3 w-full h-fit my-auto'>
+                        <p className='font-light '> Ergonomics Advance specializes in providing ergonomic assessments and evaluations to enhance workplace comfort and reduce injuries.  Their certified specialists analyze employees' behaviors, office spaces, and equipment to offer tailored recommendations aimed at improving productivity and minimizing discomfort.  Services include office ergonomic assessments, industrial ergonomics, dental ergonomics, and ergonomics training.  They serve various locations across California, including Beverly Hills, El Segundo, Hollywood, and Studio City.</p>
                         <a
                             href="#sass"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="inline-block mt-8 bg-white text-[#1A0542] px-5 py-2 rounded-md font-semibold hover:bg-gray-100 transition mb-6"
+                            className="inline-block w-fit bg-white my-6 text-[#1A0542] px-5 py-3 rounded-sm font-semibold hover:bg-gray-100 transition"
                         >
                             View Website
                         </a>
                     </div>
-                </section>                
+                </section>                               
             </div>
         </section>
 
