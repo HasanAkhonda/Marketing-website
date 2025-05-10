54c821d68a35a2ed66da07c637d1df3d849355fb
hasanakhonda <mdmahmudhasanakhonda@gmail.com>
2025-05-10 16:56:27 +0600

    fff

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import BoostYourOnlinePresence from "@/components/BoostYourOnlinePresence/BoostYourOnlinePresence";
-import ContactUs from "@/components/ContactUsSection/ContactUs";
-import FrequentlyAskedQuestion from "@/components/Frequently Asked Question/FrequentlyAskedQuestion";
-import HeroSection from "@/components/heroSection/HeroSection";
-import DifferentSection from "@/components/howAreWeDifferent/DifferentSection";
-import IncreaseROIToday from "@/components/IncreaseROItoday/IncreaseROIToday";
-import Portfolio from "@/components/portfolioSection/Portfolio";
+import BoostYourOnlinePresence from "../components/BoostYourOnlinePresence/BoostYourOnlinePresence";
+import ContactUs from "../components/ContactUsSection/ContactUs";
+import FrequentlyAskedQuestion from "../components/Frequently Asked Question/FrequentlyAskedQuestion";
+import HeroSection from "../components/heroSection/HeroSection";
+import DifferentSection from "../components/howAreWeDifferent/DifferentSection";
+import IncreaseROIToday from "../components/IncreaseROItoday/IncreaseROIToday";
+import Portfolio from "../components/portfolioSection/Portfolio";
 
 
 export default function Home() {
